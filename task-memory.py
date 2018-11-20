e4f7bc8d4bfacff2e7d699813220b4e89b65d89a
Cristovão <lucaswillian45@gmail.com>
2018-11-20 16:59:18 -0200

    First choose

--- task-memory.py
+++ task-memory.py
@@ -9,8 +9,21 @@ for i in range(100):
         memoria[i] = 'x'
     else:
         memoria[i] = ' '
-#Aqui você deve imprimir todo o conteúdo da variável memória
-
+for i in range(0,20):
+    print(memoria[i], end="|")
+print()
+for i in range(20,40):
+    print(memoria[i], end="|")
+print()
+for i in range(40,60):
+    print(memoria[i], end="|")
+print()
+for i in range(60,80):
+    print(memoria[i], end="|")
+print()
+for i in range(80,100):
+    print(memoria[i], end="|")
+print()
 while(opcao != 4):
     #Menu do programa
     print("1 - Primeira Escolha")
@@ -26,6 +39,26 @@ while(opcao != 4):
 
     if(opcao == 1):
         #Implemente aqui a lógica da primeira escolha
+        i=0
+        tamanhoburaco=0
+        possiveisLugares = [] * 0
+        proximo = False
+        while(i<100):
+            if memoria[i] == ' ':
+                tamanhoburaco = tamanhoburaco + 1;
+                possiveisLugares.append(i)
+                if tamanhoburaco == tamanho:
+                    if len(possiveisLugares) > 0:
+                        for i in range(len(possiveisLugares)):
+                            memoria[possiveisLugares[i]] = letra
+                        proximo = True
+            else:
+                tamanhoburaco = 0
+                possiveisLugares = [] * 0
+                if proximo:
+                    proximo = False
+                    break
+            i = i + 1
         pass
     else:
         if (opcao == 2):
@@ -36,3 +69,18 @@ while(opcao != 4):
                 #Implemente aqui a lógica da pior escolha
                 pass
     # Aqui você deve imprimir todo o conteúdo da variável memória
+    for i in range(0, 20):
+        print(memoria[i], end="|")
+    print()
+    for i in range(20, 40):
+        print(memoria[i], end="|")
+    print()
+    for i in range(40, 60):
+        print(memoria[i], end="|")
+    print()
+    for i in range(60, 80):
+        print(memoria[i], end="|")
+    print()
+    for i in range(80, 100):
+        print(memoria[i], end="|")
+    print()
